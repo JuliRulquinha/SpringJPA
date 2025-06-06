0371c750f40bb66ce27bca9b4f5f06e5060a08a0
JuliRulquinha <juli-felix@outlook.com>
2025-06-06 16:00:53 -0300

    Primary key generation strategies and column naming and rules strategies

--- src/main/java/com/SpringJPACourse/jpa/entities/Author.java
+++ src/main/java/com/SpringJPACourse/jpa/entities/Author.java
@@ -1,16 +1,67 @@
 package com.SpringJPACourse.jpa.entities;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 @Data
 @NoArgsConstructor
+@Entity
+@Table(name = "Author-TBL")
 public class Author {
+
+    // * VALUE GENERATION FOR PRIMARY KEYS STRATEGIES *
+    /*
+     @GeneratedValue
+
+     Using this annotation alone will result in Hibernate using AUTO by default,
+     and it will choose the best strategy for each case.
+
+    * SEQUENCE *
+
+     @GeneratedValue(
+
+        strategy = GenerationType.SEQUENCE,     - Specifies that JPA should use a database sequence
+
+        generator = "author_sequence"   - Links to the sequence generator defined below
+     )
+     @SequenceGenerator(
+
+        name = "author_sequence",       - The internal name used in JPA
+
+        sequenceName = "author_sequence",       - The actual database sequence name
+
+        allocationSize = 1      - Number of values to preallocate. Setting this to 1
+                                  means every insert will fetch a new value from the
+                                  database (less performant, but avoids gaps)
+     )
+
+    @GeneratedValue(
+        strategy = GenerationType.TABLE,
+        generator = "author_id_gen"
+    )
+    @TableGenerator(
+        name = "author_id_gen",
+        table = "id_generator",
+        pkColumnName = "id name",
+        valueColumnName = "id_value",
+        allocationSize = 1
+    )
+
+    */
+
+    @Id
+    @GeneratedValue
     private Integer id;
 
+    @Column( name = "f-name")
     private String firstName;
 
     private String lastName;
 
+    @Column(
+            unique = true,
+            nullable = false
+    )
     private String email;
 
     private int age;
