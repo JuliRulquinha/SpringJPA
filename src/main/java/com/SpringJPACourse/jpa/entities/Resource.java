5bd827dbc0b55d78de8e0ff056a3f6f7f84a169d
JuliRulquinha <juli-felix@outlook.com>
2025-06-09 17:55:34 -0300

    Created resource types to explicit what type of resource is being saved ex: video, file...

--- src/main/java/com/SpringJPACourse/jpa/entities/Resource.java
+++ src/main/java/com/SpringJPACourse/jpa/entities/Resource.java
@@ -1,17 +1,22 @@
 package com.SpringJPACourse.jpa.entities;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
-@EqualsAndHashCode(callSuper = true)
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @SuperBuilder
 @Entity
-public class Resource extends BaseEntity{
-   
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "resource_type")
+public class Resource {
+    @Id
+    @GeneratedValue
+    private Integer id;
     private String name;
     private int size;
     private String url;
---
+++ src/main/java/com/SpringJPACourse/jpa/entities/Video.java
@@ -0,0 +1,21 @@
+package com.SpringJPACourse.jpa.entities;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Inheritance;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Entity
+@DiscriminatorValue("video")
+public class Video extends Resource {
+    public int length;
+}
